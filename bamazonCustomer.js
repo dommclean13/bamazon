65402fc11d44b8ae589f7ab0fad359ec8f9c7aa3
Dominique McLean <dommclean13@gmail.com>
2019-11-26 17:34:28 -0600

    updated bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,87 @@
 var mysql = require("mysql");
 
+// connection to the database
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
+  // setting root to the default username
   user: "root",
 
-  // Your password
+  // password is an empty string
   password: "",
-  database: "bamazondatabase"
+  database: "bamazon"  
 });
 
+// connecting to the database
 connection.connect(err => {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
+
+
+// prompt the user for the quantity or item they choose
+function promptUserPurchase() {
+	// telling the user to select and item
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'id',
+			message: 'Enter the Item ID which you would like to purchase.',
+			validate: validateInput,
+			filter: Number
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many would you like?',
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
+
+		var item = input.id;
+		var quantity = input.quantity;
+
+		var queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {id: item}, function(err, data) {
+			if (err) throw err;
+
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+				displayInventory();
+
+			} else {
+				var productData = data[0];
+
+				// If the quantity requested by the user is in stock
+				if (quantity <= productData.stock) {
+					console.log('You got it! Placing order...');
+
+					// Construct the updating query string
+					var updateQueryStr = 'UPDATE products SET stock = ' + (productData.stock - quantity) + ' WHERE id = ' + item;
+					// console.log('updateQueryStr = ' + updateQueryStr);
+
+					// Update the inventory
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your total is $' + productData.price * quantity);
+						console.log("\n---------------------------------------------------------------------\n");
+
+                        displayInventory();
+					})
+				} else {
+					console.log('Not engough in stock. Try another amount');
+					console.log("\n---------------------------------------------------------------------\n");
+
+					displayInventory();
+				}
+			}
+		})
+	})
+}
+
+
